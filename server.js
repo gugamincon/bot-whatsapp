4fff8e81ca572eda607e42058592866833af8182
gugamincon <gustavomincon@gmail.com>
2026-03-10 19:22:49 -0300

    corrigindo porta render

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-// página principal
 app.get("/", (req,res)=>{
 res.send("Bot WhatsApp online 🚀");
 });
 
-// webhook que recebe mensagens do WhatsApp
 app.post("/webhook",(req,res)=>{
 console.log("Mensagem recebida:", req.body);
 res.send("ok");
 });
 
-app.listen(3000, ()=>{
-console.log("Servidor rodando");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+console.log("Servidor rodando na porta " + PORT);
 });
