24453ad75e046bd0fc3736e3f410a6d2ec904806
gugamincon <gustavomincon@gmail.com>
2026-03-10 18:54:26 -0300

    adicionando webhook

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
+// página principal
 app.get("/", (req,res)=>{
-res.send("Bot online 🚀");
+res.send("Bot WhatsApp online 🚀");
+});
+
+// webhook que recebe mensagens do WhatsApp
+app.post("/webhook",(req,res)=>{
+console.log("Mensagem recebida:", req.body);
+res.send("ok");
 });
 
 app.listen(3000, ()=>{
